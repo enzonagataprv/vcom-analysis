ebbfc5b6194ca304970abe1cc9b210c88fd2bb86
Enzo Nagata <enzo.nagata@pravaler.com.br>
2023-03-30 14:32:11 -0300

    feat: adiciona mais um datasource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,37 +3,41 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VcomReposistory } from './vcom.repository';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      name: 'pravaler',
+      type: 'postgres',
+      host: process.env.PRV_DATABASE_HOST,
+      port: +process.env.PRV_DATABASE_PORT,
+      username: process.env.PRV_DATABASE_USER,
+      password: process.env.PRV_DATABASE_PASSWORD,
+      database: process.env.PRV_DATABASE_DATABASE_NAME,
+      synchronize: true,
+    }),
     TypeOrmModule.forRoot({
       name: 'vcom',
       type: 'mssql',
-      host: '10.60.0.8',
-      port: 1433,
-      username: 'pravaler_consulta',
-      password: '65@4aLWtIjXn',
-      database: 'pravaler_cobsystems',
+      host: process.env.VCOM_DATABASE_HOST,
+      port: +process.env.VCOM_DATABASE_PORT,
+      username: process.env.VCOM_DATABASE_USER,
+      password: process.env.VCOM_DATABASE_PASSWORD,
+      database: process.env.VCOM_DATABASE_DATABASE_NAME,
       options: { encrypt: false },
       synchronize: true,
-      // synchronize: false,
-      // entities: ['project1/*.entity.ts'],
-      // subscribers: ['project1/*.subscriber.ts'],
-      // migrations: ['project1/migrations/*.ts']
     }),
     TypeOrmModule.forRoot({
       name: 'localhost',
       type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'admin',
-      password: 'admin',
-      database: 'postgres',
+      host: process.env.LOCAL_DATABASE_HOST,
+      port: +process.env.LOCAL_DATABASE_PORT,
+      username: process.env.LOCAL_DATABASE_USER,
+      password: process.env.LOCAL_DATABASE_PASSWORD,
+      database: process.env.LOCAL_DATABASE_DATABASE_NAME,
       synchronize: true,
-      // synchronize: false,
-      // entities: ['project2/*.entity.ts'],
-      // subscribers: ['project2/*.subscriber.ts'],
-      // migrations: ['project2/migrations/*.ts'],
     }),
   ],
   controllers: [AppController],
